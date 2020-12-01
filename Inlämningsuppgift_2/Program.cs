167d3f5e357d7c3976af67963debfb553fbbed92
Andrei Kozel <kozel.andrei.94@gmail.com>
2020-12-01 18:34:19 +0100

    test person description output

--- Inlämningsuppgift_2/Program.cs
+++ Inlämningsuppgift_2/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Inlämningsuppgift_2
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      var andreiK = new Person
+      {
+        age = 26,
+        astrologicalSign = AstrologicalSign.Stenbocken,
+        birthday = new DateTime(1994, 1, 12),
+        favoriteBand = "Two Steps from Hell",
+        favoriteFilm = new Dictionary<string, string>{{"title","The Lord of the Rings"},
+        {"ganre","Fantasy/Adventure"}, {"release","10 December 2001"}},
+        favoriteFood = "Carbonara",
+        fullName = "Andrei K",
+        hates = "Oliver",
+        likes = "Resa",
+        programingDrive = "Problemlösning",
+        superpower = "Instant olive detection"
+      };
+      andreiK.Describe();
     }
+  }
 }
