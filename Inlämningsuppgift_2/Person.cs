e85b69a49b4616e1010d447da072d29d4ccb953b
Andrei Kozel <kozel.andrei.94@gmail.com>
2020-12-01 22:23:45 +0100

    edit: completed Describe() method and fixed typo

--- Inlämningsuppgift_2/Person.cs
+++ Inlämningsuppgift_2/Person.cs
@@ -21,11 +21,13 @@ namespace Inlämningsuppgift_2
     public string hates;
     public AstrologicalSign astrologicalSign;
     public string superpower;
-    public string programingDrive;
+    public string programmingDrive;
 
     public void Describe()
     {
-      Console.WriteLine($"1. Namn: {fullName}, \n2. Ålder: {age} år, \n3. Födelsedag: {birthday.ToString("MMMM dd")}, \n4. Favoritmat: {favoriteFood}, \n5. Favoritband: {favoriteBand}, \n6. Favoritfilm: {favoriteFilm["title"]} - {favoriteFilm["ganre"]} - [{favoriteFilm["release"]}], \n7. Älskar: {likes}, \n8. Hatar: {hates}, \n9. Stjärntecken: {astrologicalSign}, \n10. Superpower: {superpower}, \n11. Största driv till programmering: {programingDrive}");
+      Console.Clear();
+      Console.WriteLine($"1. Namn: {fullName}, \n2. Ålder: {age} år, \n3. Födelsedag: {birthday.ToString("MMMM dd")}, \n4. Favoritmat: {favoriteFood}, \n5. Favoritband: {favoriteBand}, \n6. Favoritfilm: {favoriteFilm["title"]} - {favoriteFilm["ganre"]} - [{favoriteFilm["release"]}], \n7. Älskar: {likes}, \n8. Hatar: {hates}, \n9. Stjärntecken: {astrologicalSign}, \n10. Superpower: {superpower}, \n11. Största driv till programmering: {programmingDrive}");
+      Console.WriteLine();
     }
   }
 }
